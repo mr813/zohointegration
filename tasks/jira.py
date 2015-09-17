a7ca86116fb071cd1f419453f8af3b90cad08d75
Roman Gorodeckij <holms@holms.lt>
2015-09-17 07:53:01 +0300

    jira sketch in progress

--- tasks/jira.py
+++ tasks/jira.py
@@ -6,26 +6,7 @@ import json
 class jira:
 
     def __init__(self, **kwargs):
-        self.init_rdb(kwargs)
         self.init_jira(kwargs)
-        pass
-
-    def init_rdb(self, kwargs):
-        """ Constructor for rethinkdb"""
-
-        self.rdb_host = kwargs['rdb_host']
-        self.rdb_port = kwargs.get('rdb_port', 28015)
-        self.rdb_db = kwargs['rdb_db']
-        self.rdb_table = kwargs['rdb_table']
-
-        rdb.connect(self.rdb_host, self.rdb_port).repl()
-
-        try: # If db or table created, ignore it.
-            rdb.db_create(self.rdb_db).run()
-            rdb.db(self.rdb_db).table_create(self.rdb_table).run()
-        except Exception as e:
-            pass
-
         pass
 
 
--- tasks/main.py
+++ tasks/main.py
@@ -10,6 +10,7 @@ logging.basicConfig( \
 logger = logging.getLogger(__name__)
 
 import zoho
+import jira
 
 class zoho_to_jira:
 
@@ -17,6 +18,7 @@ class zoho_to_jira:
 
         self.init_r(kwargs)
         self.init_zoho(kwargs)
+        self.init_jira(kwargs)
 
     def init_zoho(self, kwargs):
         self.zoho = zoho.zoho_collect_tickets(
@@ -26,13 +28,22 @@ class zoho_to_jira:
             zoho_last_time=300
         )
 
+
+    def init_jira(self, kwargs):
+        """Constructor for Jira"""
+        self.jira = jira.jira(
+            jira_user='mark.lopez',
+            jira_password='regulated2015',
+        )
+
     def init_r(self, kwargs):
         """Constructor for rethinkdb"""
 
         self.r_host = kwargs['r_host']
         self.r_port = kwargs.get('r_port', 28015)
-        self.r_db = kwargs['r_db']
-        self.r_table = kwargs['r_table']
+        self.r_db = kwargs.get('r_db', 'jira')
+        self.r_table_zoho = kwargs.get('r_table_zoho', 'zoho')
+        self.r_table_jira = kwargs.get('r_table_jira', 'jira')
 
         try: # Quit if couldn't connect to db
             logger.info('Connecting to rethinkdb...')
@@ -44,11 +55,20 @@ class zoho_to_jira:
         try: # If db or table created, ignore it.
             logger.info('Creating database...')
             r.db_create(self.r_db).run()
+        except:
+            logger.warning('Database already created')
 
-            logger.info('Creating table...')
-            r.db(self.r_db).table_create(self.r_table).run()
-        except Exception as e:
-            logger.warning('Already created')
+        try:
+            logger.info('Creating table '+self.r_table_zoho)
+            r.db(self.r_db).table_create(self.r_table_zoho).run()
+        except:
+            logger.warning('Table '+self.r_table_zoho+' already created!')
+
+        try:
+            logger.info('Creating table '+self.r_table_jira)
+            r.db(self.r_db).table_create(self.r_table_jira).run()
+        except:
+            logger.warning('Table '+self.r_table_jira+' already created!')
 
 
     def save_all_tickets(self, data):
@@ -62,26 +82,34 @@ class zoho_to_jira:
 
         try:
             logger.info('Saving all zoho tickets to db...')
-            output = r.db(self.r_db).table(self.r_table)    \
+            output = r.db(self.r_db).table(self.r_table_zoho)    \
                     .insert(data, conflict="replace")   \
                     .run()
             logger.info('RethinkDB output: '+str(output))
         except Exception as e:
             logger.error('Failed to save all tickets to database! '+str(e))
 
+
+    def sync_jira(self):
+        """Sync data to Jira"""
+        logger.info("Syncing data to Jira")
+
+        pass
+
     def run(self):
-        """Sync data between zoho and jira"""
+        """Main workflow method"""
 
         # If rethinkdb is empty, we need to fetch tickets first!
         # Ignoring exception in here, as it's the only way to check
         # if table is empty without count() which is terribly slow!
         try:
-            r.db(self.r_db).table(self.r_table).nth(0).run()
+            r.db(self.r_db).table(self.r_table_zoho).nth(0).run()
             result = 1
         except:
             result = None
 
         if result is not None:
+
             logger.info('Getting recent zoho tickets...')
             data = self.zoho.get_recent_tickets()
             try:
@@ -90,16 +118,18 @@ class zoho_to_jira:
             except:
                 logger.info('There new data to sync :}')
                 self.save_all_tickets(data)
-
+                self.sync_jira()
 
         else:
             logger.info('Seems to be table is empty, fetching all zoho tickets...')
             data = self.zoho.get_all_tickets()
             self.save_all_tickets(data)
+            self.sync_jira()
 
 ztoj = zoho_to_jira(
     r_host='rethinkdb',
     r_db='zohotojira',
-    r_table='zoho'
+    r_table_zoho='zoho',
+    r_table_jira='jira'
 )
 ztoj.run()
