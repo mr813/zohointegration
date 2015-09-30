b49d899f00457a61854b36fb261c7e8851f11f98
Roman Gorodeckij <holms@holms.lt>
2015-09-30 05:23:33 +0300

    config.py: removed env vars for now

--- config.py
+++ config.py
@@ -3,34 +3,34 @@ import os
 # Zoho
 
 # Zoho portal name
-zoho_portal = os.getenv('ZOHO_PORTAL', 'icontrolusa')
+zoho_portal = 'icontrolusa'
 
 # Zoho department name
-zoho_department = os.getenv('ZOHO_DEPARTMENT', 'SBT-Client Services')
+zoho_department = 'IT'
 
 # Zoho token
-zoho_token = os.getenv('ZOHO_TOKEN', '5e32980f1c7513f0f6dcc9247d21f37a')
+zoho_token = '5e32980f1c7513f0f6dcc9247d21f37a'
 
 # Zoho last ticket update seconds ago
-zoho_last_time = int(os.getenv('ZOHO_LAST_TIME', 12000)) # 20 minutes
+zoho_last_time = int(12000) # 20 minutes
 
 # Jira
 
 # Jira user
-jira_user = os.getenv('JIRA_USER', 'mark.lopez')
+jira_user = 'mark.lopez'
 
 # Jira password
-jira_password = os.getenv('JIRA_PASSWORD', 'regulated2015')
+jira_password = 'regulated2015'
 
 # Jira project name
-jira_project = os.getenv('JIRA_PROJECT', 'ISD')
+jira_project = 'ISD'
 
 # Jira project key name
-jira_project_key = os.getenv('JIRA_PROJECT_KEY', 'ISD')
+jira_project_key = 'ISD'
 
 # RethinkDB
-r_host = os.getenv('R_HOST', 'rethinkdb')
-r_db = os.getenv('R_DB', 'zohotojira')
+r_host = 'rethinkdb'
+r_db = 'zohotojira'
 r_table_zoho = 'zoho'
 r_table_jira ='jira'
 r_tables = ['zoho', 'jira']
